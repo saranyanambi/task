c65e28a2aec0f0273d0249f5c8d35cc143406932
saranya <saranyanambi911@gmail.com>
2025-01-10 10:22:54 +0530

    Re-add frontend directory without submodule

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import { Container } from "@mui/material";
+import { TaskManager } from "./components/TaskManager";
+import { Nav } from "./components/AppBar";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Container maxWidth="lg">
+        <TaskManager />
+      </Container>
+    </>
+  );
+};
+
+export default App;
