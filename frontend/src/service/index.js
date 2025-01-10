9667b90075e852d7d2a1601881a09f6822742cd0
saranya <saranyanambi911@gmail.com>
2025-01-10 10:58:26 +0530

    backend link added

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:8082/tasks";
-const API_URL="https://task-1-clza.onrender.com/tasks"
+const API_URL = "http://localhost:8082/tasks";
+// const API_URL="https://task-1-clza.onrender.com/tasks"
 export const fetchTasks = async () => {
   const response = await axios.get(API_URL);
   return response.data;
